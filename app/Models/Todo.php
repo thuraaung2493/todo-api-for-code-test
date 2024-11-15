70ad09c0eac3d17e3741b51fce9af727b4e28ef4
thuraaung2493 <thuraaung2493@gmail.com>
2024-11-15 07:05:04 +0700

    setup model, migration and route file

---
+++ app/Models/Todo.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    /** @use HasFactory<\Database\Factories\TodoFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+
+    /**
+     * Get the attributes that should be cast.
+     * 
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'completed' => 'boolean',
+        ];
+    }
+}
